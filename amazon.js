f1569db793b52daa2713413047e7cf584a2b3094
Henrique Lambertucci <henrique.lambertucci@gmail.com>
2023-05-07 19:16:37 -0400

    Delete amazon.js

--- amazon.js
+++
@@ -1,4 +0,0 @@
-var userMood = prompt("Please enter your mood today.")
-var userName = prompt("Please enter a name.")
-
-`Once upon a time there were some intrepid explorers who wandered into the Amazon. They found some amazing things! Piranhas are more ${userMood} during the day, so make sure you cross the noun at night. Piranhas are attracted to fresh liquid and will most likely take a bite out of your body part if you verb. Whatever you do, if you have an open ${userName} try and find another way to get back to place. Good luck!`
